ccf136eda4a821a21ee893bb646bf01ff0b49133
basith1369 <basithl369@gmail.com>
2025-09-06 12:38:29 +0530

    comand

--- practice.js
+++ practice.js
@@ -1,1 +1,24 @@
-console.log("Hello,World")
+let name="Jarvis"
+console.log(name)
+
+let num=18
+console.log(num)
+
+let islogged=true
+console.log(islogged)
+
+let x;
+console.log(x)
+
+console.log(typeof name)
+console.log(typeof num)
+console.log(typeof islogged)
+console.log(typeof x)
+
+let person = {
+   student :"RDJ",
+   age : "59"
+}
+console.log(person.student)
+console.log(person.age)
+console.log(typeof person)
