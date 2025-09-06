8b62e78e1d4f32f7b6ce905a1f332b9a09597aa2
basith1369 <basithl369@gmail.com>
2025-09-06 15:12:43 +0530

    control flow

---
+++ controlFlow.js
@@ -0,0 +1,30 @@
+let Marks=85
+if(Marks>=90){
+   console.log("A+ Grade")
+}else if(Marks>=80){
+   console.log("B+ Grade") 
+}else if(Marks>=70){
+   console.log("C Grade") 
+}else{
+   console.log("Fail") 
+}
+
+let day=5
+switch(day){
+case 1: console.log("Sunday")
+          break;
+case 2: console.log("Monday")
+          break;
+case 3: console.log("Tueday")
+          break;
+case 4: console.log("Wednesday")
+          break;
+case 5 :console.log("Thursday")
+          break;                    
+case 6: console.log("Friday")
+          break;          
+case 7: console.log("Saturday")
+          break;
+default:console.log("Invalid")
+          break;
+        }
