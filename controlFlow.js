31904b19a32048c72d3ca59ad5c0b8d70df55271
basith1369 <basithl369@gmail.com>
2025-09-06 15:34:18 +0530

    control flow

--- controlFlow.js
+++ controlFlow.js
@@ -9,13 +9,13 @@ if(Marks>=90){
    console.log("Fail") 
 }
 
-let day=5
+let day=3
 switch(day){
 case 1: console.log("Sunday")
           break;
 case 2: console.log("Monday")
           break;
-case 3: console.log("Tueday")
+case 3: console.log("Tuesday")
           break;
 case 4: console.log("Wednesday")
           break;
@@ -28,3 +28,50 @@ case 7: console.log("Saturday")
 default:console.log("Invalid")
           break;
         }
+
+//Loops
+
+for(i=1;i<=3;i++){
+    console.log("welcome to DevAstra")
+}
+
+let j=1
+while(j<=3){
+    console.log("Hello World")
+    j++;
+}
+
+k=1
+do{
+   console.log("This makes the future")
+   k++
+}while(k<=3);
+
+//Arrays
+
+let colors=["Red","Blue","Green"]
+for(let i=0;i<colors.length;i++){
+    console.log(colors[i])
+}
+for(let color of colors){
+    console.log(color)
+}
+
+let person ={
+    name : "Basith",
+    age : "19",
+    college : "JNTUGV"
+}
+
+for(let key in person){
+    console.log(`${key} : ${person[key]}`)
+}
+
+for(let i=5;i>=0;i--){
+    if(i===3){
+        continue;
+    }else if(i===2){
+        break;
+    }
+    console.log(i)
+}
